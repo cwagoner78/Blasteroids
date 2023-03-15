5064f27fbba1d885eaeb513b8f0c79b347774970
ClintWagoner <cwagoner78@gmail.com>
2023-03-15 13:55:04 -0500

    cleaned up score code

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,24 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEngine.UI;
-using UnityEditor.VersionControl;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private TMP_Text _scoreText;
     [SerializeField] private int _score;
 
-    void Update()
-    {
-        _scoreText.text = "Score: " + _score.ToString();
-
-    }
-
     public void AddScore(int points)
     {
         _score += points;
+        _scoreText.text = "Score: " + _score.ToString();
 
     }
 }
