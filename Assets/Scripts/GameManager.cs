25d4652b59e8c5e76448ee551c4b261e56721674
ClintWagoner <cwagoner78@gmail.com>
2023-03-16 13:24:53 -0500

    Added title screen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,8 +13,19 @@ public class GameManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.R) && _isGameOver)
         {
-            SceneManager.LoadScene(0); //Game Scene
+            SceneManager.LoadScene(1); //Game Scene
         }
+
+        if (Input.GetKeyDown(KeyCode.Q) && _isGameOver)
+        {
+            SceneManager.LoadScene(0); //Main Menu
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
     }
 
 
