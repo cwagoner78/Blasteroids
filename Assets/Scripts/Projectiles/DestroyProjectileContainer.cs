05df8544a3bd9943bcab525cc76af9e64af67224
ClintWagoner <cwagoner78@gmail.com>
2023-03-23 07:50:33 -0500

    tweaked lasers and containers

--- Assets/Scripts/Projectiles/DestroyProjectileContainer.cs
+++ Assets/Scripts/Projectiles/DestroyProjectileContainer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TripleShot : MonoBehaviour
+public class DestroyProjectileContainer : MonoBehaviour
 {
     void Start()
     {
