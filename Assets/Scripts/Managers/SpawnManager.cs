f4d13c7d905cc651696ce949bf252b6c2532ec41
ClintWagoner <cwagoner78@gmail.com>
2023-04-06 11:06:08 -0500

    Created enemy movement that dodges objects except player. I also updated the enemy shoot function to only happen when the player or an asteroid is in the path of the enemy.

--- Assets/Scripts/Managers/SpawnManager.cs
+++ Assets/Scripts/Managers/SpawnManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class SpawnManager : MonoBehaviour
 {
