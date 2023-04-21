a728f9bb8bf024d6caa8afaff13dde33418029a8
ClintWagoner <cwagoner78@gmail.com>
2023-04-21 11:40:54 -0500

    Fixed enemy dodge to be more natural

--- Assets/Scripts/Collectibles/PowerUp.cs
+++ Assets/Scripts/Collectibles/PowerUp.cs
@@ -1,7 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting.FullSerializer;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PowerUp : MonoBehaviour
