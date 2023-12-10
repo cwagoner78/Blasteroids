d9c001535a3481eddeeda63cbdaf836383679216
Clint Wagoner <clint@astralclocktowerstudios.com>
2023-12-10 13:42:42 -0600

    Removed test comment from AudioManager.cs

--- Assets/Scripts/Sound/AudioManager.cs
+++ Assets/Scripts/Sound/AudioManager.cs
@@ -27,8 +27,6 @@ public class AudioManager : MonoBehaviour
         else Destroy(gameObject);
         
         DontDestroyOnLoad(gameObject);
-
-        //TEST
     }
 
     public void StartTitleMusic()
